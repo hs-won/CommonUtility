e101ddf6c9dc24c09bf33fef4c848dbf0c54d350
원형식 <hyeongsik.won@navercorp.com>
2023-10-10 19:44:28 +0900

    test

--- Sources/CommonUtility/CommonUtility.swift
+++ Sources/CommonUtility/CommonUtility.swift
@@ -1,5 +1,5 @@
 public enum CommonUtility {
-    public static var text: String = "Hello, world!"
+    public static var text: String = "Hello, world."
     public static func testRun() {
         print(Self.text)
     }
