bdfdb8f0d286cae9ede9c87c51de6efcc8658427
원형식 <hyeongsik.won@navercorp.com>
2023-10-10 19:42:20 +0900

    test

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CommonUtility",
-    platforms: [.iOS(.v13), .macOS(.v10_12)],
+    platforms: [.iOS(.v13), .macOS(.v10_13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
